3e8077759277004d1a2661ba656e74d3ae9f06e8
Maksym Kalashnyk <maxplay123890@gmail.com>
2024-05-21 10:24:49 +0300

    first commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='header'>
+      <div className="header__log">Log</div>
+      <div className="header__sign">Sign</div>
+    </div>
+  )
+}
+
+export default Navbar
